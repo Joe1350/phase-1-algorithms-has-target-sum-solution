a68c563e7bc3338b22667a7d4b1bd04976073292
Joseph Martin <josephmartin1350@gmail.com>
2021-12-12 20:27:43 -0700

    commit tree d3a8c347d5a82c49cd0c75521bcfd2a9f6af12c6

--- index.js
+++ index.js
@@ -1,18 +1,46 @@
+  // optimized and cleaned
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  const seenNumbers = {}
+  for (let number of array) {
+    const complement = target - number
+    if (complement in seenNumbers) {
+      return true
+    }
+    seenNumbers[number] = true
+  }
+  return false
 }
+
 
 /* 
   Write the Big O time complexity of your function here
+
+  runtime: O(n)
+  space: O(n)
 */
+
 
 /* 
   Add your pseudocode here
+
+  create an object to keep track of numbers we've already seen
+
+  iterate through each number in the array
+    for the current number identify what number adds up to the target
+    check if any numbers in the object are the compliment
+      if so, return true
+      otherwise add number to the object
+  if I get to the end of the array and haven't found my numbers, return false
 */
+
 
 /*
   Add written explanation of your solution here
+
+  make a function called hasTargetSum that checks if 2 numbers from the array add up to some target.
+  for each number check if there's a number that adds up to the target
 */
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
@@ -32,3 +60,31 @@ if (require.main === module) {
 }
 
 module.exports = hasTargetSum;
+
+
+  // original
+// function hasTargetSum(array, target) {
+//   for (i = 0; i < array.length; i++) {
+//     const complement = target - array[i]
+//     for (let j = i + 1; j < array.length; j++) {
+//       if (array[j] === complement) {
+//         return true
+//       }
+//     }
+//   }
+//   return false
+// }
+
+
+  // optimized
+// function hasTargetSum(array, target) {
+//   const seenNumbers = {}
+//   for (let i = 0; i < array.length; i++) {
+//     const complement = target - array[i]
+//     if (seenNumbers[complement]) {
+//       return true
+//     }
+//     seenNumbers[array[i]] = true
+//   }
+//   return false
+// }
